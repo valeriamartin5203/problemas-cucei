9f003f7785a26a936d3ad8ebed0f7c2325dbca61
valeriamartin5230 <valeria.martin5203@alumnos.udg.mx>
2026-03-15 17:05:55 -0600

    cambio de nombre y agregar el login y register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import Login from "./login"
-import Register from "./registre"
+import Register from "./register"
 import Reporte from "./reporte"
 
 function App(){
